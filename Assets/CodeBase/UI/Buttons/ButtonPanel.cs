edfe03fd48bbd56dafa69612bc49e487023bb473
Dron26 <91642819+Dron26@users.noreply.github.com>
2023-10-27 17:29:20 +0300

    Add Panel Shower EnemyCount

--- Assets/CodeBase/UI/Buttons/ButtonPanel.cs
+++ Assets/CodeBase/UI/Buttons/ButtonPanel.cs
@@ -1,33 +1,41 @@
 using System;
 using Infrastructure.BaseMonoCache.Code.MonoCache;
+using Service.SaveLoad;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI.Buttons
 {
-    public class ButtonPanel: MonoCache
+    public class ButtonPanel : MonoCache
     {
         [SerializeField] private Button _menu;
         [SerializeField] private GameObject _rightPanel;
         [SerializeField] private GameObject _leftPanel;
+        [SerializeField] private GameObject _enemyCountPanel;
+        [SerializeField] private GameObject _additionPanel;
+            
+        private CountEnemyPanel _countEnemyPanel;
+        private bool isActive = true;
 
-        private bool isActive=true;
-
-        private void Start()
+        public void Initialize(SaveLoadService saveLoadService)
         {
-              InitializeButton();
+            _countEnemyPanel = _enemyCountPanel.GetComponentInChildren<CountEnemyPanel>();
+            _countEnemyPanel.Initialize(saveLoadService);
+            InitializeButton();
         }
 
         private void InitializeButton()
         {
+            _menu.onClick.AddListener(SwitchPanelState);
         }
 
         public void SwitchPanelState()
         {
-            isActive=!isActive;
+            isActive = !isActive;
             _rightPanel.SetActive(isActive);
             _leftPanel.SetActive(isActive);
+            _enemyCountPanel.gameObject.SetActive(isActive);
+            _additionPanel.SetActive(isActive);
         }
-        
     }
 }
